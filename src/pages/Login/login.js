4e65bfa7ba7ce15aa6af8ae8a660e1d00666458e
srsexton94 <srsexton94@gmail.com>
2021-05-23 16:10:22 -0400

    Finish up MVP Login page

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,18 +1,32 @@
 import './login.scss'
 import { useContext, useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FirebaseContext } from '../../context/firebase'
+import { Routes } from '../../constants/routes'
 
 export default function Login() {
   const history = useHistory()
   const { firebase } = useContext(FirebaseContext)
 
-  const [emailAdress, setEmailAddress] = useState('')
+  const [emailAddress, setEmailAddress] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
-  const isInvalid = password === '' || emailAdress ===''
+  
+  const isEmailValid = emailAddress && emailAddress.match(/^\w+@{1}\w+[.]{1}([a-zA-Z]{2,3})$/gi)
+  const isPasswordValid = password && password.length >= 6
+  const isFormInvalid =  !isPasswordValid || !isEmailValid
 
-  const handleLogin = () => {}
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(Routes.DASHBOARD);
+    } catch (error) {
+      setEmailAddress('');
+      setPassword('');
+      setError(error.message);
+    }
+  }
 
   useEffect(() => {
     document.title = 'Login - SesameGram'
@@ -21,14 +35,34 @@ export default function Login() {
   return (
     <section className="login-section">
       <h1 className="login-title">SesameGram - Login:</h1>
-      { error && <p>{ error }</p> }
-      <form className="signin-form" onSubmit={handleLogin} method="POST">
-        <input type="text" placeholder="Email address" aria-label="Enter email address"/>
-        <input type="text" placeholder="Password" aria-label="Enter password"/>
-        <button type="submit">
-          Submit
+      { error && <p className="error-message">{ error }</p> }
+      <form className="signin-form" onSubmit={ handleLogin } method="POST">
+        <input
+          type="email" 
+          placeholder="Email address" 
+          aria-label="Enter email address"
+          aria-invalid={ !isEmailValid }
+          onChange={({ target }) => setEmailAddress(target.value)}
+          value={ emailAddress }
+        />
+        <input
+          type="password" 
+          placeholder="Password" 
+          aria-label="Enter password"
+          aria-invalid={ !isPasswordValid }
+          onChange={({ target }) => setPassword(target.value)}
+          value={ password }
+        />
+        <button type="submit" disabled={ isFormInvalid }>
+          Login
         </button>
       </form>
+      <div className="signup-redirect">
+        <p>Don't have an account yet?</p>
+        <Link className="signup-link" to={ Routes.SIGNUP }>
+          Sign up now!
+        </Link>
+      </div>
     </section>
   )
 }
